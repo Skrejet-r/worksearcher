13bcfaf43ce9f3363b54cead00fcefa389586b35
Maxim Göring <mg0307m@gmail.com>
2020-11-25 21:07:44 +0100

    some structure

--- bot.py
+++ bot.py
@@ -1,3 +1,21 @@
 import config
 import logging
 
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from sqldb import dbfuncs
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+bot = Bot(token=config.API_TOKEN)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+db = dbfuncs("db.db")
+
+
+@
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
