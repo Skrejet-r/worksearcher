f050477124417cdad2c7aacb298c0604ac81a975
Maxim Göring <mg0307m@gmail.com>
2020-11-29 22:20:46 +0100

    f-start

--- bot.py
+++ bot.py
@@ -11,10 +11,17 @@ logger = logging.getLogger(__name__)
 bot = Bot(token=config.API_TOKEN)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
-db = dbfuncs("db.db")
+db = dbfuncs("db_ws.db")
 
 
-@
+@dp.message_handler(commands=["start"])
+async def welcome(message: types.Message):
+    if not (db.user_exists(message.from_user.id)):
+        await message.answer("Welcome")
+        # if user isnt in the db - add him
+        db.add_user(message.from_user.id)
+        await message.answer("I see you are new here!"
+                             "\nPress /lang to set language for yourself ")
 
 
 if __name__ == "__main__":
--- sqldb.py
+++ sqldb.py
@@ -6,5 +6,14 @@ class dbfuncs:
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
+    def user_exists(self, user_id):  # proof if it is here
+        with self.connection:
+            return self.cursor.execute('SELECT * FROM "users" WHERE "user_id"=?', (user_id,)).fetchall()
+
+    def add_user(self, user_id):  # add new user
+        with self.connection:
+            return self.cursor.execute(
+                'INSERT INTO "users" ("user_id") VALUES(?)', (user_id,))
+
     def close(self):
         self.connection.close()
